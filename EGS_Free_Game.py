bc1cea9a8694e88ef334fb1f92c634f395d023c3
RazielX <RazielX2012@gmail.com>
2021-07-04 07:24:49 +0300

    Добавлен readme.md

--- EGS_Free_Game.py
+++ EGS_Free_Game.py
@@ -9,9 +9,8 @@ options = webdriver.FirefoxOptions()
 options.set_preference('dom.webdriver.enabled', False)
 # options.headless = True   # Активация фонового режима
 
-# options.add_argument(
-#     'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36  (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-# )
+# options.add_argument( 'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36  (KHTML, like Gecko)
+# Chrome/89.0.4389.90 Safari/537.36', )
 
 url = 'https://www.epicgames.com/store/ru'
 url_login = 'https://www.epicgames.com/id/login/epic'
@@ -21,6 +20,7 @@ browser = webdriver.Firefox(options=options)
 
 def login_egs():
     """Вход в аккаунт"""
+    print('Authorization')
     browser.get(url_login)
     try:
         time_to_wait = WebDriverWait(browser, 20).until(
@@ -40,11 +40,11 @@ def login_egs():
     finally:
         sign_in_button = browser.find_element_by_id('sign-in')
         sign_in_button.click()
-        print('Authorization')
 
 
 def return_to_main_page():
     """ Возвращение на главную страницу"""
+    print('Return to main page')
     try:
         time_to_wait = WebDriverWait(browser, 20).until(
             ec.element_to_be_clickable((By.CLASS_NAME, "shieldLogo "))
@@ -52,19 +52,18 @@ def return_to_main_page():
     finally:
         sign_in_button = browser.find_element_by_class_name('shieldLogo')
         sign_in_button.click()
-        print('Return to main page')
 
 
 def search_free_weekly():
     """Поиск еженедельной бесплатной игры"""
-    free_weekly = browser.find_elements_by_class_name('css-1ihd7u3')
-    print(free_weekly)
-    browser.find_element_by_class_name('css-1ihd7u3').click()
     print('Searching for free weekly game')
+    free_weekly = browser.find_elements_by_class_name('css-1ihd7u3')
+    browser.find_element_by_class_name('css-1ihd7u3').click()
 
 
 def claim_free_game():
     """ Забираем бесплатную игру """
+    print('Claim free game')
     try:
         time_to_wait = WebDriverWait(browser, 20).until(
             ec.element_to_be_clickable((By.XPATH,
@@ -106,4 +105,3 @@ if __name__ == '__main__':
     claim_free_game()
     # browser.get(url)
     # browser.quit()
-
